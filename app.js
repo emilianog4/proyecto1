8e21cb370dfc5dd83724c0d3758c66f78ff187d9
facundonahuelg26 <facundonahuelg26@gmail.com>
2024-07-06 15:25:33 -0300

    [fgutierrez] add new js

---
+++ app.js
@@ -0,0 +1,21 @@
+/* Ejercicio 1 */
+const name = prompt('What is your name?')
+alert(`Hello ${name}!`)
+
+/* Ejercicio 2 */
+
+const person = {
+  name: 'John',
+  age: 26,
+  country: 'USA',
+  jobs: ['engineer', 'developer']
+}
+
+console.log('Imprimir nombre')
+console.log('Imprimir edad')
+console.log('Imprimir Pais')
+console.log('Imprimir trabajos')
+
+/* Ejercicio 3 */
+
+// Utilizar el objeto person
